4263bbe4d9c6dc251723f871b3f4f18cf52f93ff
Shell-thon <shellterminal0@gmail.com>
2023-07-15 04:14:53 +0100

    update

---
+++ 0x10-https_ssl/git-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pull changes from Git repository
+git pull
+
+# Add all files
+git add .
+
+# Commit changes with the message "update"
+git commit -m "update"
+
+# Push changes to Git repository
+git push
